2828e0c5f2a594c8b9dda7bf943325e6825a7f6b
VladRybalka <vlad.contact2035@gmail.com>
2026-02-11 11:17:12 +0200

    Add new sport in Sports.db
    Add new sport in Sports.db with name and fields of this sport

--- server/Logic/work_with_databases.py
+++ server/Logic/work_with_databases.py
@@ -2,13 +2,12 @@ import sqlite3
 
 # Создание базы данных по году(если её нет) и создание таблицы по спорту с нужными полями.
 def create_empty_year_db(year, sport, fields):
-    con = sqlite3.connect(f'Data\\{year}.db')
+    con = sqlite3.connect(f'Data\\workout_data\\{year}.db')
     cur = con.cursor()
 
     sqlRequest = f"CREATE TABLE IF NOT EXISTS {sport}("
-    for i in fields:
-        sqlRequest += f"{i},"
+    for field in fields:
+        sqlRequest += f"{field},"
     sqlRequest = sqlRequest[:-1] + ")"
-    print(sqlRequest)
 
     cur.execute(sqlRequest)
