5e44fe83389e7fbeb9e727f9fae38daf6e979eda
VladRybalka <vlad.contact2035@gmail.com>
2026-02-13 17:22:08 +0200

    Add types
    Bug in add_train

--- server/Logic/work_with_databases.py
+++ server/Logic/work_with_databases.py
@@ -5,6 +5,10 @@ def add_sport(name, fields):
     con = sqlite3.connect('Data\\sports_data\\sports.db')
     cur = con.cursor()
 
+    sql_request = f"CREATE TABLE IF NOT EXISTS sports(id INTEGER PRIMARY KEY AUTOINCREMENT, sport_name, fields)"
+    cur.execute(sql_request)
+    con.commit()
+
     # Отправляет запрос на добавление и применяет его.
     cur.execute(f'INSERT INTO sports(sport_name, fields) VALUES("{name}", "{fields}")')
     con.commit()
@@ -22,14 +26,6 @@ def get_fields(sport):
 
     return fields[0]
 
-# def get_count_fields(sport):
-#     con = sqlite3.connect('Data\\sports_data\\sports.db')
-#     cur = con.cursor()
-#     cur.execute(f'SELECT fields FROM sports WHERE sport_name = "{sport}"')
-#     fields = cur.fetchall()
-#     con.close()
-#     return len(str(fields).split(",")) - 1
-
 # Создание базы данных по году(если её нет) и создание таблицы по спорту с нужными полями.
 def create_empty_year_db(year, sport):
     con = sqlite3.connect(f'Data\\workout_data\\{year}.db')
@@ -39,7 +35,6 @@ def create_empty_year_db(year, sport):
     sql_request = f"CREATE TABLE IF NOT EXISTS {sport}(id INTEGER PRIMARY KEY AUTOINCREMENT, "
     for i in get_fields(sport):
         sql_request += f"{i},"
-        print(i)
     sql_request = sql_request[:-1] + ")"
 
     # Отправляет запрос на создание таблицы и применяет его.
@@ -49,6 +44,8 @@ def create_empty_year_db(year, sport):
 
 # Добавление тренировки.
 def add_train(year, sport, data):
+    create_empty_year_db(year, sport)
+
     con = sqlite3.connect(f'Data\\workout_data\\{year}.db')
     cur = con.cursor()
 
--- server/server.py
+++ server/server.py
@@ -4,12 +4,13 @@ if __name__ == "__main__":
     while 1:
         menu = int(input("0 - добавить спорт, 1 - добавить тренировку: "))
         if menu == 0:
-            sport = input("Введеди название: ")
+            sport = input("Введите название: ")
             fields = input("Введите названия характеристик: ")
             database_logic.add_sport(sport, fields)
         elif menu == 1:
             sport = input("Введеди название: ")
             enter = []
-            for i in database_logic.get_fields("Cycling")[0].split(","):
-                enter.append(input(i.strip() + ": "))
+            field = database_logic.get_fields("Cycling")[0].split(",")
+            for i in range(len(field)):
+                enter.append(input(field[i].strip() + ": "))
             database_logic.add_train(2025, sport, enter)
