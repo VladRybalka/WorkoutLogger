c02ec4f4dac2c0fe017fa1b5024d0fc1265339c6
VladRybalka <vlad.contact2035@gmail.com>
2026-02-13 16:53:24 +0200

    Add comments

--- server/Logic/work_with_databases.py
+++ server/Logic/work_with_databases.py
@@ -8,17 +8,23 @@ def add_sport(name, fields):
     con.commit()
     con.close()
 
-# Получение полей для вида спорта.
+# Получение полей вида спорта.
 def get_fields(sport):
     con = sqlite3.connect('Data\\sports_data\\sports.db')
     cur = con.cursor()
-
     cur.execute(f'SELECT fields FROM sports WHERE sport_name = "{sport}"')
     fields = cur.fetchall()
-    print(fields)
     con.close()
 
     return fields[0]
+
+# def get_count_fields(sport):
+#     con = sqlite3.connect('Data\\sports_data\\sports.db')
+#     cur = con.cursor()
+#     cur.execute(f'SELECT fields FROM sports WHERE sport_name = "{sport}"')
+#     fields = cur.fetchall()
+#     con.close()
+#     return len(str(fields).split(",")) - 1
 
 # Создание базы данных по году(если её нет) и создание таблицы по спорту с нужными полями.
 def create_empty_year_db(year, sport):
@@ -32,5 +38,19 @@ def create_empty_year_db(year, sport):
     sql_request = sql_request[:-1] + ")"
     print(sql_request)
     cur.execute(sql_request)
+    con.commit()
+    con.close()
+
+# Добавление тренировки.
+def add_train(year, sport, data):
+    con = sqlite3.connect(f'Data\\workout_data\\{year}.db')
+    cur = con.cursor()
+
+    request = f"INSERT INTO {sport}({get_fields(sport)[0]}) VALUES("
+    for i in data:
+        request += f"{i},"
+    request = request[:-1] + ")"
+
+    cur.execute(request)
     con.commit()
     con.close()
--- server/server.py
+++ server/server.py
@@ -1,4 +1,15 @@
 import Logic.work_with_databases as database_logic
 
 if __name__ == "__main__":
-    database_logic.create_empty_year_db(2025, "Cycling")
+    while 1:
+        menu = int(input("0 - добавить спорт, 1 - добавить тренировку: "))
+        if menu == 0:
+            sport = input("Введеди название: ")
+            fields = input("Введите названия характеристик: ")
+            database_logic.add_sport(sport, fields)
+        elif menu == 1:
+            sport = input("Введеди название: ")
+            enter = []
+            for i in database_logic.get_fields("Cycling")[0].split(","):
+                enter.append(input(i.strip() + ": "))
+            database_logic.add_train(2025, sport, enter)
