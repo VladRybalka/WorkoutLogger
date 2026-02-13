29af662fe51d70eb34a3db28232b5460b9e96ff0
VladRybalka <vlad.contact2035@gmail.com>
2026-02-13 14:15:54 +0200

    Add train and create test in Python

--- server/Logic/work_with_databases.py
+++ server/Logic/work_with_databases.py
@@ -1,23 +1,36 @@
 import sqlite3
 
+# Добавления нового вида спорта в базу данных.
 def add_sport(name, fields):
     con = sqlite3.connect('Data\\sports_data\\sports.db')
     cur = con.cursor()
-    sqlRequest = f'INSERT INTO sports(sport_name, fields) VALUES("{name}", "{fields}")'
-    cur.execute(sqlRequest)
+    cur.execute(f'INSERT INTO sports(sport_name, fields) VALUES("{name}", "{fields}")')
     con.commit()
     con.close()
 
+# Получение полей для вида спорта.
+def get_fields(sport):
+    con = sqlite3.connect('Data\\sports_data\\sports.db')
+    cur = con.cursor()
+
+    cur.execute(f'SELECT fields FROM sports WHERE sport_name = "{sport}"')
+    fields = cur.fetchall()
+    print(fields)
+    con.close()
+
+    return fields[0]
+
 # Создание базы данных по году(если её нет) и создание таблицы по спорту с нужными полями.
-def create_empty_year_db(year, sport, fields):
+def create_empty_year_db(year, sport):
     con = sqlite3.connect(f'Data\\workout_data\\{year}.db')
     cur = con.cursor()
 
-    sqlRequest = f"CREATE TABLE IF NOT EXISTS {sport}(id INTEGER PRIMARY KEY AUTOINCREMENT, "
-    for field in fields:
-        sqlRequest += f"{field},"
-    sqlRequest = sqlRequest[:-1] + ")"
-
-    cur.execute(sqlRequest)
+    sql_request = f"CREATE TABLE IF NOT EXISTS {sport}(id INTEGER PRIMARY KEY AUTOINCREMENT, "
+    for i in get_fields(sport):
+        sql_request += f"{i},"
+        print(i)
+    sql_request = sql_request[:-1] + ")"
+    print(sql_request)
+    cur.execute(sql_request)
     con.commit()
     con.close()
--- server/server.py
+++ server/server.py
@@ -1,5 +1,4 @@
 import Logic.work_with_databases as database_logic
 
 if __name__ == "__main__":
-    # database_logic.create_empty_year_db(2025, "Cicle", ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"])
-    database_logic.add_sport("Cycling", "Time, Distance, Average_Speed, KCalorie")
+    database_logic.create_empty_year_db(2025, "Cycling")
