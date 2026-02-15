6f275de7abf9ca0036f6e32966ed92ea97ae3379
VladRybalka <vlad.contact2035@gmail.com>
2026-02-15 12:30:06 +0200

    Fix bug

--- server/Logic/work_with_databases.py
+++ server/Logic/work_with_databases.py
@@ -5,12 +5,12 @@ def add_sport(name, fields):
     con = sqlite3.connect('Data\\sports_data\\sports.db')
     cur = con.cursor()
 
-    sql_request = f"CREATE TABLE IF NOT EXISTS sports(id INTEGER PRIMARY KEY AUTOINCREMENT, sport_name, fields)"
+    sql_request = f"CREATE TABLE IF NOT EXISTS sports(id INTEGER PRIMARY KEY AUTOINCREMENT, sport_name TEXT, fields TEXT)"
     cur.execute(sql_request)
     con.commit()
 
     # Отправляет запрос на добавление и применяет его.
-    cur.execute(f'INSERT INTO sports(sport_name, fields) VALUES("{name}", "{fields}")')
+    cur.execute(f'INSERT INTO sports(sport_name, fields) VALUES("{name}", "{"Day INTEGER, Month INTEGER, " + fields}")')
     con.commit()
     con.close()
 
@@ -21,10 +21,10 @@ def get_fields(sport):
 
     # Отправляет запрос и получает ответ в виде [(элемент)].
     cur.execute(f'SELECT fields FROM sports WHERE sport_name = "{sport}"')
-    fields = cur.fetchall()
+    answer = cur.fetchall()
     con.close()
 
-    return fields[0]
+    return answer[0][0]
 
 # Создание базы данных по году(если её нет) и создание таблицы по спорту с нужными полями.
 def create_empty_year_db(year, sport):
@@ -32,10 +32,8 @@ def create_empty_year_db(year, sport):
     cur = con.cursor()
 
     # Формирует SQL-запрос.
-    sql_request = f"CREATE TABLE IF NOT EXISTS {sport}(id INTEGER PRIMARY KEY AUTOINCREMENT, "
-    for i in get_fields(sport):
-        sql_request += f"{i},"
-    sql_request = sql_request[:-1] + ")"
+    sql_request = (f"CREATE TABLE IF NOT EXISTS {sport}(id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                   f"Day INTEGER, Month INTEGER, {get_fields(sport)})")
 
     # Отправляет запрос на создание таблицы и применяет его.
     cur.execute(sql_request)
@@ -50,12 +48,10 @@ def add_train(year, sport, data):
     cur = con.cursor()
 
     # Формирует запрос на добавление тренировки.
-    request = f"INSERT INTO {sport}({get_fields(sport)[0]}) VALUES("
-    for i in data:
-        request += f"{i},"
-    request = request[:-1] + ")"
+    fields_name = ",".join(get_fields(sport).split(" ")[::2])
+    sql_request = f"INSERT INTO {sport}({fields_name}) VALUES(" + ",".join(data) + ")"
 
     # Отправляет запрос и применяет его.
-    cur.execute(request)
+    cur.execute(sql_request)
     con.commit()
     con.close()
--- server/server.py
+++ server/server.py
@@ -2,15 +2,19 @@ import Logic.work_with_databases as database_logic
 
 if __name__ == "__main__":
     while 1:
-        menu = int(input("0 - добавить спорт, 1 - добавить тренировку: "))
+        menu = int(input("0 - добавить спорт, 1 - добавить тренировку, 2 - посмотреть поля спорта,"
+                         " 3 - получить тренировку по дате: "))
         if menu == 0:
             sport = input("Введите название: ")
             fields = input("Введите названия характеристик: ")
             database_logic.add_sport(sport, fields)
         elif menu == 1:
-            sport = input("Введеди название: ")
+            sport = input("Введите название: ")
             enter = []
-            field = database_logic.get_fields("Cycling")[0].split(",")
+            field = database_logic.get_fields(sport).split(",")
             for i in range(len(field)):
                 enter.append(input(field[i].strip() + ": "))
             database_logic.add_train(2025, sport, enter)
+        elif menu == 2:
+            sport = input("Введите название: ")
+            print(database_logic.get_fields(sport))
