4a282353b43fd47f2b646a49fc3d3185e84cf235
VladRybalka <vlad.contact2035@gmail.com>
2026-03-01 13:33:08 +0200

    Add C#
    Create C# application

---
+++ server/server.py
@@ -0,0 +1,21 @@
+import flask
+
+app = flask.Flask(__name__)
+
+@app.route('/add_sport')
+def add_sport():
+    pass
+
+@app.route('/add_train')
+def add_train():
+    pass
+
+@app.route('/get_train')
+def get_train():
+    pass
+
+@app.route('/test')
+def test():
+    return "OK"
+
+app.run(port=5000)
