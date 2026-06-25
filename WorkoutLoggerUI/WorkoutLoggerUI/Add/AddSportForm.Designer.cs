d37df21f1f752f973798982456ecebbd4c1bcf94
VladRybalka <vlad.contact2035@gmail.com>
2026-06-25 18:31:24 +0300

    Add Sport BL

--- WorkoutLoggerUI/WorkoutLoggerUI/Add/AddSportForm.Designer.cs
+++ WorkoutLoggerUI/WorkoutLoggerUI/Add/AddSportForm.Designer.cs
@@ -28,46 +28,133 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.components = new System.ComponentModel.Container();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.btn_cancel = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btn_addColumn = new System.Windows.Forms.Button();
+            this.btn_deleteColumn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_name = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // btn_add
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(13, 388);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(136, 50);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "ADD";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btn_add.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_add.Location = new System.Drawing.Point(12, 452);
+            this.btn_add.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(141, 42);
+            this.btn_add.TabIndex = 0;
+            this.btn_add.Text = "ADD";
+            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
             // 
-            // button2
+            // btn_cancel
             // 
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button2.Location = new System.Drawing.Point(155, 388);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(136, 50);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Cancel";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btn_cancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_cancel.Location = new System.Drawing.Point(161, 452);
+            this.btn_cancel.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_cancel.Name = "btn_cancel";
+            this.btn_cancel.Size = new System.Drawing.Size(141, 42);
+            this.btn_cancel.TabIndex = 1;
+            this.btn_cancel.Text = "Cancel";
+            this.btn_cancel.UseVisualStyleBackColor = true;
+            this.btn_cancel.Click += new System.EventHandler(this.btn_cancel_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 44);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(599, 363);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // btn_addColumn
+            // 
+            this.btn_addColumn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_addColumn.Location = new System.Drawing.Point(12, 414);
+            this.btn_addColumn.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_addColumn.Name = "btn_addColumn";
+            this.btn_addColumn.Size = new System.Drawing.Size(30, 30);
+            this.btn_addColumn.TabIndex = 3;
+            this.btn_addColumn.Text = "+";
+            this.btn_addColumn.UseVisualStyleBackColor = true;
+            this.btn_addColumn.Click += new System.EventHandler(this.btn_addColumn_Click);
+            // 
+            // btn_deleteColumn
+            // 
+            this.btn_deleteColumn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_deleteColumn.Location = new System.Drawing.Point(52, 414);
+            this.btn_deleteColumn.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_deleteColumn.Name = "btn_deleteColumn";
+            this.btn_deleteColumn.Size = new System.Drawing.Size(30, 30);
+            this.btn_deleteColumn.TabIndex = 4;
+            this.btn_deleteColumn.Text = "-";
+            this.btn_deleteColumn.UseVisualStyleBackColor = true;
+            this.btn_deleteColumn.Click += new System.EventHandler(this.btn_deleteColumn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Name:";
+            // 
+            // txt_name
+            // 
+            this.txt_name.Location = new System.Drawing.Point(88, 6);
+            this.txt_name.Name = "txt_name";
+            this.txt_name.Size = new System.Drawing.Size(144, 32);
+            this.txt_name.TabIndex = 6;
+            this.txt_name.Enter += new System.EventHandler(this.txt_name_Enter);
+            this.txt_name.Leave += new System.EventHandler(this.txt_name_Leave);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // AddSportForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 26F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(624, 519);
+            this.Controls.Add(this.txt_name);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_deleteColumn);
+            this.Controls.Add(this.btn_addColumn);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_cancel);
+            this.Controls.Add(this.btn_add);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
             this.Name = "AddSportForm";
             this.Text = "AddSportForm";
+            this.Load += new System.EventHandler(this.AddSportForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.Button btn_cancel;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btn_addColumn;
+        private System.Windows.Forms.Button btn_deleteColumn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_name;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- WorkoutLoggerUI/WorkoutLoggerUI/Add/AddSportForm.cs
+++ WorkoutLoggerUI/WorkoutLoggerUI/Add/AddSportForm.cs
@@ -16,5 +16,105 @@ namespace WorkoutLoggerUI
         {
             InitializeComponent();
         }
+
+        #region -==- Buttons -==-
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        #region -==- Column -==-
+
+        private void btn_addColumn_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Add();
+        }
+
+        private void btn_deleteColumn_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.RemoveAt(dataGridView1.CurrentCell.RowIndex);
+        }
+
+        #endregion
+
+        private void btn_cancel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        #endregion
+
+        #region -==- TextBoxName Events -==-
+
+        private void txt_name_Enter(object sender, EventArgs e)
+        {
+            if(txt_name.Text == "Example: Run")
+            {
+                txt_name.Text = "";
+            }
+            txt_name.BackColor = Color.White;
+            txt_name.ForeColor = Color.Black;
+        }
+
+        private void txt_name_Leave(object sender, EventArgs e)
+        {
+            if(txt_name.Text == "")
+            {
+                txt_name.Text = "Example: Run";
+                txt_name.BackColor = Color.FromArgb(230, 230, 230);
+                txt_name.ForeColor = Color.FromArgb(100, 100, 100);
+            }
+        }
+
+        #endregion
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (txt_name.Text == "Example: Run" || txt_name.Text == "")
+            {
+                dataGridView1.Enabled = false;
+                btn_addColumn.Enabled = false;
+                btn_deleteColumn.Enabled = false;
+                btn_add.Enabled = false;
+            }
+            else
+            {
+                dataGridView1.Enabled = true;
+                btn_addColumn.Enabled = true;
+                btn_deleteColumn.Enabled = true;
+                btn_add.Enabled = true;
+            }
+        }
+
+        private void AddSportForm_Load(object sender, EventArgs e)
+        {
+            txt_name_Leave(sender, e);
+
+            DataGridViewComboBoxCell list = new DataGridViewComboBoxCell();
+            list.Items.Add("Numeric");
+            list.Items.Add("Decimal");
+            list.Items.Add("Text");
+
+            DataGridViewColumn column1 = new DataGridViewColumn();
+            column1.HeaderText = "Name";
+            column1.Width = dataGridView1.Width / 2 - 10;
+            column1.CellTemplate = new DataGridViewTextBoxCell();
+
+            DataGridViewColumn column2 = new DataGridViewColumn();
+            column2.HeaderText = "Characteristics";
+            column2.Width = dataGridView1.Width / 2 - 10;
+            column2.CellTemplate = list;
+            
+
+            dataGridView1.Columns.Add(column1);
+            dataGridView1.Columns.Add(column2);
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.RowHeadersVisible = false;
+
+            timer1.Start();
+        }
+
+        
     }
 }
